fc0a9cb8333d719899afe4706b929b2f8e90f975
TheJoker971 <jojo.blsegor@gmail.com>
2025-02-05 16:35:58 +0100

    Deploy my contracts

--- ETH/script/JokerToken.s.sol
+++ ETH/script/JokerToken.s.sol
@@ -2,15 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {JokerToken} from "../src/JokerToken.sol";
 
-contract CounterScript is Script {
+contract JokerTokenScript is Script {
+
+    JokerToken public token;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-
-
+        token = JokerToken(0xC554cb653Dd31ba84BdC250e6Ca5a0fA7dB97E46);
         vm.stopBroadcast();
     }
 }
