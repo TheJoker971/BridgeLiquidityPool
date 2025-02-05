b58bb05c13b62ab51af3cf55d058f001d10f423f
TheJoker971 <jojo.blsegor@gmail.com>
2025-02-05 15:39:31 +0100

    Create my Vault contract

---
+++ ETH/src/Vault.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+contract TokenVault {
+
+    // Variables Globales
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public starts;
+
+    // Evénements
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+
+    // Errors
+    error INSUFFICIENT_AMOUNT(uint256);
+    error USER_ALREADY_DEPOSIT(address);
+    error INSUFFICIENT_BALANCE();
+
+    // Fonction de dépôt
+    function deposit(address user, uint256 amount) external {
+        // Assurez-vous que le montant est non nul
+        require(amount > 0, INSUFFICIENT_AMOUNT(amount));
+        require(balances[user] == 0 && starts[user] == 0,USER_ALREADY_DEPOSIT(user));
+
+        // Mise à jour du solde de l'utilisateur
+        balances[user] += amount;
+        starts[user] += block.timestamp;
+        // Emission de l'événement de dépôt
+        emit Deposit(user, amount);
+    }
+
+    // Fonction de retrait
+    function withdraw(address user, uint256 amount) external {
+        // Vérifier que l'utilisateur a assez de fonds pour le retrait
+        require(balances[user] >= amount,INSUFFICIENT_BALANCE());
+
+        // Le retrait peut être sécurisé avec un "proof" (preuve) pour valider l'opération, 
+        // cette logique peut être étendue selon vos besoins (par exemple, pour les "Merkle proofs").
+        // Ici, nous ignorons l'implémentation réelle de la preuve pour simplifier.
+
+        // Mise à jour du solde de l'utilisateur
+        balances[user] -= amount;
+
+        // Emission de l'événement de retrait
+        emit Withdraw(user, amount);
+
+        // Si le contrat a des fonds en ETH ou autres tokens, les envoyer à l'utilisateur
+        // Si c'est un contrat ERC20, vous pouvez appeler la fonction `transfer` sur le token.
+        payable(user).transfer(amount);
+    }
+
+    // Fonction pour obtenir le solde en ETH du contrat
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    // Fonction permettant de recevoir des ethers dans le contrat
+    receive() external payable {}
+}
+
